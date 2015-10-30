147f7fd7c7d396ddea927f1dcd30d2308faa0119
Jonathan Lange <jml@mumak.net>
2015-10-30 14:07:18 +0000

    Explain what I'm going to do

--- src/Main.hs
+++ src/Main.hs
@@ -3,5 +3,20 @@ module Main (main) where
 import BasicPrelude
 
 
+-- 1. Get documentation for package:
+--
+-- $ ghc-pkg field --simple-output turtle haddock-html
+-- /nix/store/n3rgkskgjdph4ykqi845z5ayspzqbfqr-turtle-1.2.2/share/doc/x86_64-osx-ghc-7.10.2/turtle-1.2.2/html
+
+-- 2. Get documentation for module
+--
+-- $ ghc-pkg find-module --simple-output Turtle
+-- turtle-1.2.2
+
+-- 3. Open documentation
+-- $ open $PATH/index.html
+-- $ xdg-open $PATH/index.html
+
+
 main :: IO ()
 main = putStrLn "Hello world!"
